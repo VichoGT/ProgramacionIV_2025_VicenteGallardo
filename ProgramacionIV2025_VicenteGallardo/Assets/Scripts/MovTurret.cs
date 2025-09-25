44e3e4732f9a38806ded9f5c76c8b6428cbdfcd7
VichoGT <vicente.gallardo@zonavirtual.uisek.cl>
2025-09-24 22:34:12 -0300

    Cambios Al input System y integrar el mov de la turret

---
+++ ProgramacionIV2025_VicenteGallardo/Assets/Scripts/MovTurret.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovTurret : MonoBehaviour
+{
+    public InputActionAsset action;
+    private InputAction rotateAction;
+
+
+    public float rotateSpd;
+    private Vector2 rotatePos;
+    private Rigidbody2D rb;
+    private void OnEnable()
+    {
+        action.FindActionMap("Player").Enable();
+    }
+    private void OnDisable()
+    {
+        action.FindActionMap("Player").Disable();
+    }
+    private void Awake()
+    {
+        rotateAction = InputSystem.actions.FindAction("MovementTurret"); //Mantener el nombre de la accion del InputSystem
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        rotatePos = rotateAction.ReadValue<Vector2>();
+    }
+    private void FixedUpdate()
+    {
+        RotateMove();
+    }
+    void RotateMove()
+    {
+        if (rotatePos.x != 0)
+        {
+            float rotationPos = -rotatePos.x * rotateSpd * Time.deltaTime;
+            transform.Rotate(0, 0, rotationPos);
+        }
+    }
+}
